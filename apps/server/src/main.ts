6ea151161c4767a21023818559bdd8196a51c68a
Neil <morrison.neil.v@gmail.com>
2026-03-03 12:40:20 -0500

    added back swagger module

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -8,6 +9,15 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Utilitx API')
+    .setDescription('Underground asset record management API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(port);
   console.log(`Server running on http://localhost:${port}`);
   console.log(`Swagger docs at http://localhost:${port}/api/docs`);
